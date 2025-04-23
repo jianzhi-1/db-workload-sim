eec49d851adf35cb91de1ffa2fb957c5433bd372
jianzhi-1 <jianzhi@berkeley.edu>
2025-04-22 17:44:11 -0700

    added correlated workload idea

--- Workload.py
+++ Workload.py
@@ -10,15 +10,13 @@ def draw_zipf_with_max(a, max_value):
     return 1
 
 class Workload(ABC):
-    def __init__(self) -> None:
-        pass
 
     @abstractmethod
     def generate_random_transactions(self, num_txns:int, probabilities:list[float], start=0) -> list[Transaction]:
         ...
 
 class SmallBankWorkload(Workload):
-    def __init__(self) -> None:
+    def __init__(self, correlated:bool=False, corr_params:dict=None) -> None:
 
         self.NUM_ACCOUNTS = 10
         
@@ -44,12 +42,39 @@ class SmallBankWorkload(Workload):
 
         self.SMALLBANK_HOT_KEYS = [(0,i) for i in range(10)] + [(1, i) for i in range(10)] + [(2, i) for i in range(10)] # ???
 
+        self.correlated:bool = correlated
+        self.corr_params:dict = corr_params
+        self.ttl = None # time to next event
+        self.state = 0 # 0: random, 1: correlated
+        self.sticky_value = None # the value that is correlated to during that time interval
+
     def gen(self, typ:str) -> int:
         if typ in ["long", "string"]: return draw_zipf_with_max(1.01, self.NUM_ACCOUNTS)
         elif typ in "double": return random.randint(0, 100)
         assert False, f"Error: type {typ} not supported in gen"
 
+    def generate_transactions(self, num_txns:int, probabilities:list[float]=None, start=0) -> list[Transaction]:
+        if not self.correlated: return self.generate_random_transactions(num_txns, probabilities=probabilities, start=start)
+        else:
+            import numpy as np
+            res = None
+            if self.ttl is None or (self.ttl == 0 and self.state == 1) or (self.ttl > 0 and self.state == 0):
+                self.state = 0
+                if self.ttl is None or self.ttl == 0: self.ttl = max(1, int(np.random.exponential(scale=self.corr_params["normal_scale"], size=None)))
+                res = self.generate_random_transactions(num_txns, probabilities=probabilities, start=start)
+            elif (self.ttl == 0 and self.state == 0) or (self.ttl > 0 and self.state == 1):
+                self.state = 1
+                if self.ttl == 0: 
+                    self.ttl = max(1, int(np.random.exponential(scale=self.corr_params["event_scale"], size=None)))
+                    self.sticky_value = None
+                res = self.generate_correlated_transactions(num_txns, probabilities=probabilities, start=start, p=self.corr_params["correlation_factor"])
+            else:
+                assert False, "unreachable code"
+            self.ttl -= 1
+            return res
+
     def generate_random_transactions(self, num_txns:int, probabilities:list[float]=None, start=0) -> list[Transaction]:
+        # start argument is used to deduplicate transaction ids when multiple time steps are scheduled
 
         if probabilities is None: probabilities = [1/len(self.SMALLBANK_TXN_OPS)] * len(self.SMALLBANK_TXN_OPS) # assume by default uniform
 
@@ -68,6 +93,34 @@ class SmallBankWorkload(Workload):
                 else: op_list.append(ReadOperation(txn_id, (op[1], val))) # read operation
             txn_arr.append(Transaction(txn_id, op_list))
 
+        return txn_arr
+
+    def generate_correlated_transactions(self, num_txns:int, probabilities:list[float]=None, start=0, p=0.1) -> list[Transaction]:
+        # start argument is used to deduplicate transaction ids when multiple time steps are scheduled
+
+        if probabilities is None: probabilities = [1/len(self.SMALLBANK_TXN_OPS)] * len(self.SMALLBANK_TXN_OPS) # assume by default uniform
+
+        txn_type_arr = random.choices(population=list(self.SMALLBANK_TXN_OPS.keys()), weights=probabilities, k=num_txns)
+        
+        txn_arr = []
+        
+        for i, v in enumerate(txn_type_arr):
+            txn_id = start + i
+            op_list = []
+            cache = dict()
+            for op in self.SMALLBANK_TXN_OPS[v]:
+                val = None
+                if op[2] in cache:
+                    val = cache[op[2]] # no choice, must take the cache value
+                elif self.sticky_value is not None and random.random() < p: # for correlatedness
+                    val = self.sticky_value
+                else:
+                    val = self.gen(self.SMALLBANK_INPUT_TYPINGS[v][op[2]])
+                    if self.sticky_value is None: self.sticky_value = val
+                if op[2] not in cache: cache[op[2]] = val
+                if op[0]: op_list.append(WriteOperation(txn_id, (op[1], val))) # write operation
+                else: op_list.append(ReadOperation(txn_id, (op[1], val))) # read operation
+            txn_arr.append(Transaction(txn_id, op_list))
         return txn_arr
 
 if __name__ == "__main__":
--- scripts/script_ksmf_correlated.py
+++ scripts/script_ksmf_correlated.py
@@ -2,7 +2,12 @@ from Simulator import Simulator
 from Scheduler import KSMFScheduler
 from Workload import SmallBankWorkload
 
-workload = SmallBankWorkload()
+corr_params = {
+    "normal_scale": 10.0,
+    "event_scale": 20.0,
+    "correlation_factor": 0.1
+}
+workload = SmallBankWorkload(correlated=True, corr_params=corr_params)
 probabilities = [0.15, 0.15, 0.15, 0.25, 0.15, 0.15] # https://github.com/cmu-db/benchbase/blob/main/config/mysql/sample_smallbank_config.xml#L22
 
 scheduler = KSMFScheduler(k=5)
@@ -22,4 +27,3 @@ while not sim.done():
 
 sim.print_statistics()
 
-# {'n_aborts': 203, 'n_successes': 9797, 'steps': 14330}
